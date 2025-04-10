ba36f2ef5053a38841abd4730fe662805411e8dd
rahulkumarbhagat-coder <bhagatrahul847@gmail.com>
2025-04-10 15:45:58 +0530

    update website

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,21 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+  const modal = document.getElementById("contactModal");
+  const openBtn = document.getElementById("openModal");
+  const closeBtn = document.querySelector(".close");
+
+  openBtn.onclick = () => {
+    modal.style.display = "block";
+  };
+
+  closeBtn.onclick = () => {
+    modal.style.display = "none";
+  };
+
+  window.onclick = (e) => {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  };
